aa7bd549f18d9e707d965d19d6ae5a02dcc33391
venkatesh1169 <31930862+venkatesh1169@users.noreply.github.com>
2017-09-13 18:47:19 +0000

    Add files via upload

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -143,18 +143,49 @@ public class StudentGroup implements StudentArrayOperation {
 	}
 
 	@Override
-	public void removeFromElement(Student student) {
+	public void removeFromElement(Student student) throws IllegalArgumentException{
 		// Add your implementation here
+		int index=-1;
+		if(student==null)
+		    throw new IllegalArgumentException();
+		int size=students.length;
+		for(int i=0;i<size;i++)
+		    if(students[i].equals(student))
+		          index=i;
+		    if(index!=-1)
+		   {
+		       removeFromIndex(index);
+		    }
+		    return;
 	}
 
 	@Override
-	public void removeToIndex(int index) {
+	public void removeToIndex(int index) throws IllegalArgumentException{
 		// Add your implementation here
+		int j=0;
+		if(index<0||index>=students.length)
+		     throw new IllegalArgumentException();
+		    Student[] a=new Student[students.length-index];
+		    for(int i=index;i<students.length;i++)
+		        a[j++]=students[i];
+		    students=a;
 	}
 
 	@Override
-	public void removeToElement(Student student) {
+	public void removeToElement(Student student) throws IllegalArgumentException{
 		// Add your implementation here
+		int index=-1;
+		if(student==null)
+		    throw new IllegalArgumentException();
+		int size=students.length;
+		for(int i=0;i<size;i++)
+		    if(students[i].equals(student))
+		          index=i;
+		    if(index!=-1)
+		   {
+		       removeToIndex(index);
+		    }
+		    return;
 	}
 
 	@Override
