3243bd77b760b3201964350e8e0e5c1e37cd43c8
venkatesh1169 <31930862+venkatesh1169@users.noreply.github.com>
2017-09-13 16:55:06 +0000

    Add files via upload

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -66,7 +66,8 @@ public class StudentGroup implements StudentArrayOperation {
 	@Override
 	public void add(Student student, int index) throws IllegalArgumentException{
 		// Add your implementation here
-		students[index].add(student);
+		for(int i=index+1
+		students.add(index,student);
 		if(index<0||index>=students.length)
 		     throw new IllegalArgumentException();
 	}
